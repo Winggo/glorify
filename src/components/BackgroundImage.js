ddd831b845fc2ae82f4b199c6480cd741f0f20f6
Winggo Tse <winggo.tse@gmail.com>
2025-10-26 21:53:20 -0700

    simply resize background image using only css

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,39 +1,7 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React from 'react';
 import './BackgroundImage.css';
 
 function BackgroundImage({ image }) {
-	const [orientation, setOrientation] = useState("landscape");
-	const imgRef = useRef(null);
-
-	const updateDimensions = useCallback(() => {
-		if (imgRef.current) {
-			const windowHeight = window.innerHeight;
-			const imageHeight = imgRef.current.height;
-			
-			if (windowHeight > imageHeight) {
-				setOrientation("portrait");
-			} else {
-				setOrientation("landscape");
-			}
-		}
-	}, []);
-
-	useEffect(() => {
-		updateDimensions();
-		window.addEventListener("resize", updateDimensions);
-		
-		return () => {
-			window.removeEventListener("resize", updateDimensions);
-		};
-	}, [updateDimensions]);
-
-	useEffect(() => {
-		// Update dimensions when image loads
-		if (imgRef.current) {
-			imgRef.current.onload = updateDimensions;
-		}
-	}, [image, updateDimensions]);
-
 	if (!image) {
 		return null;
 	}
@@ -41,10 +9,9 @@ function BackgroundImage({ image }) {
 	return (
 		<div>
 			<img 
-				ref={imgRef}
 				src={image} 
 				id="unsplash" 
-				className={orientation} 
+				className="background-image"
 				alt="Beautiful landscape"
 				loading="lazy"
 			/>
